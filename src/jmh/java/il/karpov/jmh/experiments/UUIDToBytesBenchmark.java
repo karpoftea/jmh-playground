161fe10ec6a2805e92031278920ab2af7127353d
Ilya Karpov <i.karpov@cleverdata.ru>
2019-05-23 13:04:40 +0300

    Add UUIDToBytes benchmark

---
+++ src/jmh/java/il/karpov/jmh/experiments/UUIDToBytesBenchmark.java
@@ -0,0 +1,47 @@
+package il.karpov.jmh.experiments;
+
+import java.nio.ByteBuffer;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Warmup;
+
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.SECONDS)
+@Warmup(iterations = 5)
+@Measurement(iterations = 5)
+public class UUIDToBytesBenchmark {
+
+    private static final String TEST_UUID = UUID.randomUUID().toString();
+    private static final byte[] TEST_UUID_BYTES = asBytes(TEST_UUID);
+
+    @Benchmark
+    public byte[] asBytes() {
+        return asBytes(TEST_UUID);
+    }
+
+    @Benchmark
+    public String asUuidString() {
+        return asUuidString(TEST_UUID_BYTES);
+    }
+
+    private static String asUuidString(byte[] bytes) {
+        ByteBuffer bb = ByteBuffer.wrap(bytes);
+        long firstLong = bb.getLong();
+        long secondLong = bb.getLong();
+        return new UUID(firstLong, secondLong).toString();
+    }
+
+    private static byte[] asBytes(String uuid) {
+        UUID id = UUID.fromString(uuid);
+        ByteBuffer bb = ByteBuffer.wrap(new byte[16]);
+        bb.putLong(id.getMostSignificantBits());
+        bb.putLong(id.getLeastSignificantBits());
+        return bb.array();
+    }
+}
